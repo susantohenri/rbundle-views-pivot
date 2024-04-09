cb776624c38e339705e52f700b3dd69c4b8454a7
Henri Susanto <henry.dinus@gmail.com>
2024-04-09 10:23:54 +0700

    preparation

--- rbundle-views-pivot.php
+++ rbundle-views-pivot.php
@@ -11,7 +11,7 @@
  * @wordpress-plugin
  * Plugin Name: RBundle Views Pivot
  * Plugin URI:  https://github.com/susantohenri/rbundle-views-pivot
- * Description: RBundle Views Pivot
+ * Description: When you need to show data in views base on logged in user (with faster loading & sortable)
  * Version:     1.0.0
  * Author:      Henri Susanto
  * Author URI:  https://github.com/susantohenri/
@@ -19,3 +19,35 @@
  * License:     GPL v2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  */
+
+define('RBUNDLE_VIEWS_PIVOT_CONFIG', [
+    58 => [
+        'star_shortlist',
+        'your_proposal',
+        'compatibility',
+        'star_shortlist',
+        'user_rating',
+    ]
+]);
+
+register_activation_hook(__FILE__, function () {
+});
+
+register_deactivation_hook(__FILE__, function () {
+});
+
+add_action('frm_after_create_entry', function ($entry_id, $form_id) {
+}, 30, 2);
+
+add_action('frm_after_update_entry', function ($entry_id, $form_id) {
+}, 10, 2);
+
+add_action('frm_before_destroy_entry', function ($entry_id) {
+});
+
+add_filter('frm_view_order', function ($query, $args) {
+}, 10, 2);
+
+add_shortcode('rbundle-pivot-value', function ($atts) {
+    $user_id = get_current_user_id();
+});
