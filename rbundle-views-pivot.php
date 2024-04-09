8317f254a533e6c17f14641605229011bbd1415f
Henri Susanto <henry.dinus@gmail.com>
2024-04-09 09:51:20 +0700

    init

---
+++ rbundle-views-pivot.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * RBundle Views Pivot
+ *
+ * @package     RBundleViewsPivot
+ * @author      Henri Susanto
+ * @copyright   2022 Henri Susanto
+ * @license     GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name: RBundle Views Pivot
+ * Plugin URI:  https://github.com/susantohenri/rbundle-views-pivot
+ * Description: RBundle Views Pivot
+ * Version:     1.0.0
+ * Author:      Henri Susanto
+ * Author URI:  https://github.com/susantohenri/
+ * Text Domain: RBundleViewsPivot
+ * License:     GPL v2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
